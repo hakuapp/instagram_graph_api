6822d2d96824e8ba04a08023289abffbbd84c62c
leo-haku <leo@hakusports.com>
2020-08-26 16:45:28 -0400

    Add limit to media query - set default limit of max

--- lib/instagram_graph_api/client/hash_tag.rb
+++ lib/instagram_graph_api/client/hash_tag.rb
@@ -7,7 +7,7 @@ module InstagramGraphApi
       def get_tag_id(name)
         accounts = ig_business_accounts
         user_id  = accounts.first['id'] if accounts.any?
-        uri = "ig_hashtag_search?user_id=#{user_id}&q=#{name}"
+        uri      = "ig_hashtag_search?user_id=#{user_id}&q=#{name}"
         puts "graph call get tags: #{uri}"
         graph_call(uri).first
       rescue Exception => e
@@ -15,32 +15,32 @@ module InstagramGraphApi
         return nil
       end
 
-      def recent_tag_media(tag_id, fields = nil)
-        get_media tag_id, 'recent', fields
+      def recent_tag_media(tag_id, fields = nil, limit = 50)
+        get_media tag_id, 'recent', fields, limit
       end
 
-      def recent_tag_media_by_name(name, fields = nil)
+      def recent_tag_media_by_name(name, fields = nil, limit = 50)
         tag_id_response = get_tag_id(name)
-        tag_id_response ? get_media(tag_id_response["id"], 'recent', fields) : []
+        tag_id_response ? get_media(tag_id_response["id"], 'recent', fields, limit) : []
       end
 
-      def top_tag_media(tag_id, fields = nil)
-        get_media tag_id, 'top', fields
+      def top_tag_media(tag_id, fields = nil, limit = 50)
+        get_media tag_id, 'top', fields, limit
       end
 
-      def top_tag_media_by_name(name, fields = nil)
+      def top_tag_media_by_name(name, fields = nil, limit = 50)
         tag_id_response = get_tag_id(name)
-        tag_id_response ? get_media(tag_id_response["id"], 'top', fields) : []
+        tag_id_response ? get_media(tag_id_response["id"], 'top', fields, limit) : []
       end
 
       private
 
-      def get_media(tag_id, type, fields)
+      def get_media(tag_id, type, fields, limit = 50)
         fields   ||= MEDIA_DEFAULT_FIELDS
         accounts = ig_business_accounts
         user_id  = accounts.first['id'] if accounts.any?
         puts "user_id: #{user_id}, tag_id: #{tag_id}"
-        get_connections(tag_id, "#{type}_media?user_id=#{user_id}&fields=#{fields}")
+        get_connections(tag_id, "#{type}_media?user_id=#{user_id}&fields=#{fields}&limit=#{limit}")
       end
 
     end
